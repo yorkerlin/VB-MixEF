3c43a033b8a0449154dc0eef064228a2342c2232
yorkerlin <yorker.lin@gmail.com>
2019-09-18 00:21:11 -0700

    add the toy example for GMM

---
+++ src/matlab/gmm/gmm_demo.m
@@ -0,0 +1,111 @@
+% reproduce the plots in the paper
+close all; clear all;
+clc
+
+setSeed(1);
+
+% settings
+nrSteps=1e5;
+
+% load data
+load('./datasets/cancermortality.mat')
+
+y=double(cancermortality.y);
+n=double(cancermortality.n);
+
+logp0 = @(theta)BetaBinomialLogPosterior(theta,y,n);
+
+% define log likelihood
+logPostDens = @(x)BetaBinomialLogPosterior(x,y,n);
+logPostDensQuad = @(x1,x2)BetaBinomialLogPosteriorQuad(x1,x2,y,n);
+gradfun = @(x)BetaBinomialGradFun(x,y,n);
+
+% contour grid
+[theta1,theta2] = meshgrid(-7.7:.02:-5.6,4.5:0.1:14);
+cvals = [-0.15 -0.5 -1 -2 -3 -5];
+Z=zeros(size(theta1));
+
+figure()
+subplot(3,3,9)
+for i=1:size(theta1,1)
+    for j=1:size(theta1,2)
+        Z(i,j)=logPostDens([theta1(i,j);theta2(i,j)]);
+    end
+end
+mzr = max(max(Z));
+normconst = dblquad(@(x1,x2)exp(logPostDensQuad(x1,x2)-mzr),-10,-3.5,2,20);
+Z=Z-mzr-log(normconst);
+[C,h] = contour(theta1,theta2,Z,cvals);
+colormap jet
+xlabel('logit u');
+ylabel('log P');
+title('exact')
+true_dist.theta1= theta1;
+true_dist.theta2= theta2;
+true_dist.Z =Z;
+
+
+likelihoodFun = @(xSampled,iter)evaluate_likelihood(xSampled,iter,logPostDens,gradfun);
+
+init_m=zeros(2,1);
+init_P=eye(2);
+
+nrSamples = 500000;
+k = length(init_m);
+rawNorm = randn(k,nrSamples);
+rawUnif = rand(nrSamples,1);
+
+num_com = 8;
+results=cell(num_com,3);
+
+% try multiple mixture components
+for i=1:num_com
+    disp(['Estimating approximation with ' int2str(i) ' components.'])
+    nrComponents=i;
+
+    % fit approximation
+    [mixWeights,mixMeans,mixPrecs] = mix_gauss_cvi(likelihoodFun,nrComponents,nrSteps,init_m,init_P);
+
+    % approximation density
+    myDens=@(x1,x2)DensApproximation(x1,x2,mixWeights,mixMeans,mixPrecs);
+    myLogDens=@(x1,x2)log(myDens(x1,x2));
+
+    % contour plot approximation
+    subplot(3,3,i)
+    for l=1:size(theta1,1)
+        for j=1:size(theta1,2)
+            Z(l,j)=myLogDens(theta1(l,j),theta2(l,j));
+        end
+    end
+
+    [C,h] = contour(theta1,theta2,Z,cvals);
+    %[C,h] = contourf(theta1,theta2,Z,10);
+    colormap jet
+    xlabel('logit u');
+    ylabel('log P');
+    title(int2str(i))
+    results{nrComponents,1} = theta1;
+    results{nrComponents,2} = theta2;
+    results{nrComponents,3} = Z;
+end
+
+
+function [lp,grad,hess] = evaluate_likelihood(xSampled,iter,logPostFun,gradfun)
+%iter is a dummy index in this example
+[k,nrSamples] = size(xSampled);
+lp = zeros(nrSamples,1);
+grad = zeros(k,nrSamples);
+if nargout>2
+    hess = zeros(k,k,nrSamples);
+end
+
+for j=1:nrSamples
+    lp(j) = logPostFun(xSampled(:,j));
+    if nargout>2
+        [grad(:,j),hess(:,:,j)] = gradfun(xSampled(:,j));
+    else
+        grad(:,j) = gradfun(xSampled(:,j));
+    end
+end
+end
+
---
+++ src/matlab/gmm/src/mix_gauss_cvi.m
@@ -0,0 +1,173 @@
+function [mixWeights,mixMeans,mixPrecs] = mix_gauss_cvi(likelihoodFun,nrComponents,nrSteps,init_m,init_P,callbackFun,dataset_name, preIt, nrSamples, stepSize, decay_mix, beta1)
+
+if nargin>11
+    file_name = sprintf('%s-my_method-%d-%.8f-%.8f-%.8f.mat',dataset_name,nrComponents,stepSize,decay_mix,beta1)
+    callbackInfo = zeros(nrSteps,2);
+end
+
+% algorithm settings
+if nargin<7
+    preIt=floor(nrSteps/50);
+    stepSize = 0.0005; %saliman's 2d
+    nrSamples = nrComponents*2;
+    beta1 = 0;
+    decay_mix = 0.01;
+end
+assert(beta1 == 0);
+
+% dimension
+k = size(init_m,1);
+xMode = init_m;
+xNegHess = init_P;
+
+cholNegHess = chol(xNegHess);
+mixMeans = cell(nrComponents,1);
+mixPrecs = cell(nrComponents,1);
+
+for c=1:nrComponents
+    mixMeans{c} = xMode + (cholNegHess\randn(k,1));
+    mixPrecs{c} = xNegHess;
+end
+
+mixWeights = ones(1,nrComponents)/nrComponents;
+
+%gradient
+gmu = cell(nrComponents,1);
+gV = cell(nrComponents,1);
+for c=1:nrComponents
+    gmu{c} = 0*mixMeans{c};
+    gV{c} = 0*mixPrecs{c};
+end
+
+if nrComponents>1
+    tlam_w = log( mixWeights(1,1:end-1) ) - log( mixWeights(1,end) );
+    g_mean_m_w = zeros(1,nrComponents-1);
+    g_var_m_w = zeros(1,nrComponents-1);
+else
+    tlam_w = 0;
+end
+logMixWeights = log(mixWeights);
+
+% do stochastic approximation
+for i=1:(nrSteps)
+    if nrComponents>1
+        max_tlam_w = max(max(tlam_w),0);
+        norm_log_w = log1p( sum( exp(tlam_w - max_tlam_w) )-1+ exp(-max_tlam_w) ) + max_tlam_w;
+        logMixWeights(1,1:end-1) = tlam_w - norm_log_w;
+        logMixWeights(1,end) = -norm_log_w;
+    end
+
+    %perform inference
+
+    [zSampled,logSampDensPerComp] = SampleFromMixture(logMixWeights,mixMeans,mixPrecs,nrSamples);
+    %zSampled = z ~ q(z) (generate samples from q(z))
+    %logSampDensPerComp = log( q(z|w) )
+
+    [logRBindicator,logTotalSampDens] = CombineMixtureComponents(logMixWeights,logSampDensPerComp);
+    %logRBindicator = log( q(w|z^*) )
+    %logTotalSampDens =log( q(z^*) )
+
+    %compute log likelihood with prior  log p(z^*,x)
+    [lpDens,grad,hess] = likelihoodFun(zSampled,i);
+    grad = num2cell(grad, [1])';
+    hess = squeeze( num2cell(hess,[1,2]) );
+
+    log_sxxWeights = logMixWeights';%log( q(w) )
+
+    %sxyWeights = matrixProd(logRBindicator,(lpDens-logTotalSampDens')/nrSamples);
+    %%sxyWeights = E_{q(w|z)} [ log p(z,x) - log q(z)  ]
+    [log_sxyWeights sig]= logMatrixProd(logRBindicator,(lpDens-logTotalSampDens')/nrSamples);
+
+    g_m_w2 = exp(log_sxyWeights  - log_sxxWeights) .* sig;
+    g_m_w2(sig==0) = 0;
+    g_m_w2= g_m_w2';
+
+    %the following double loop can be improved by using parallelism
+    for c=1:nrComponents
+        sH = zeros(k,k);
+        sa = zeros(k,1);
+
+        for j=1:nrSamples
+            %compute the entropy term
+            [grb,hrb] = neg_log_gmm(zSampled(:,j),logRBindicator(:,j),mixMeans,mixPrecs,c);
+            % grb = - \nabla_z log q(z)
+            % hrb = - \nabla_z^2 log q(z)
+
+            lwt = logRBindicator(c,j) - log_sxxWeights(c);
+            sa = sa + weightProd(lwt,grad{j}+grb);
+            sH = sH + weightProd(lwt,hess{j}+hrb);
+        end
+        gmu{c} = sa/nrSamples;
+        gV{c} = sH/(2*nrSamples);
+    end
+
+    lr1 = stepSize;
+
+    %line search for stepsize
+    c = 1;
+    while 1
+        try
+            while c<=nrComponents
+                chol( mixPrecs{c} - (2*lr1*(gV{c})) );
+                c = c+1;
+            end
+        catch
+            lr1 = lr1*0.5;
+            continue;
+        end
+        break
+    end
+
+    for c=1:nrComponents
+        mixPrecs{c} = mixPrecs{c} - 2*lr1*(gV{c});
+        mixMeans{c} = mixMeans{c} + lr1*(mixPrecs{c}\gmu{c});
+    end
+
+    lr2 = lr1*decay_mix;
+
+    %decrease weight rwt at each iteration
+    beta1 = 1.0-1e-8;
+    decay1 = (1.0-beta1)/(1.0-power(beta1,i));
+    rwt = 5e5*decay1;
+
+    if nrComponents>1
+        g_m_w = g_m_w2(1,1:end-1) - g_m_w2(1,end);
+        %tlam_w = tlam_w + lr2*g_m_w; %without entropy regularization
+        tlam_w = (1.0-rwt*lr2)*tlam_w + lr2*g_m_w; %add an entropy regularization ( rwt * E_q{w}[ -log q(w) ] )
+    end
+
+    if nargin>5 && mod(i,preIt) == 0
+
+        if nrComponents>1
+            max_tlam_w = max(max(tlam_w),0);
+            norm_log_w = log1p( sum( exp(tlam_w - max_tlam_w) )-1+ exp(-max_tlam_w) ) + max_tlam_w;
+            mixWeights(1,1:end-1) = exp(tlam_w - norm_log_w);
+            mixWeights(1,end) = 1-sum(mixWeights(1,1:end-1));
+        end
+
+        post_dist.mixWeights=mixWeights;
+        post_dist.mixMeans=mixMeans;
+        post_dist.mixPrecs=mixPrecs;
+
+        if nargin>11
+            callbackInfo(i,:) = callbackFun(i,post_dist);
+        else
+            callbackFun(i,post_dist);
+        end
+
+    end
+end
+
+if nrComponents>1
+    max_tlam_w = max(max(tlam_w),0);
+    norm_log_w = log1p( sum( exp(tlam_w - max_tlam_w) )-1+ exp(-max_tlam_w) ) + max_tlam_w;
+    logMixWeights(1,1:end-1) = tlam_w - norm_log_w;
+    logMixWeights(1,end) = -norm_log_w;
+    mixWeights = exp( logMixWeights);
+end
+
+if nargin>11
+    save(file_name,'preIt','nrSamples','stepSize','beta1','decay_mix','nrComponents','nrSteps','init_m','init_P','callbackInfo');
+end
+
+end
