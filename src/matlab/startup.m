a3336c3f7d33663fe50bdbf6d9c7caa87ba34e53
yorkerlin <yorker.lin@gmail.com>
2019-09-18 00:41:50 -0700

    update

--- src/matlab/startup.m
+++ src/matlab/startup.m
@@ -1,6 +1,2 @@
-% startup script to make Octave/Matlab aware of the GPML package
-%
-% Copyright (c) by Carl Edward Rasmussen and Hannes Nickisch 2018-05-17.
-
 disp('executing startup script...')
 addpath(genpath(pwd))
